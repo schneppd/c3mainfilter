e7fc7798160fd980728cac2f90ff4bac1645431c
schneppd <david.schnepp@schneppd.com>
2016-09-17 16:53:50 +0200

    Test uninstall sql v0.1

--- sql/uninstall.sql
+++ sql/uninstall.sql
@@ -1,5 +1,5 @@
-DROP TABLE `PREFIX_c3_mainfilter_selection_group_shelf`;
-DROP TABLE `PREFIX_c3_mainfilter_selection_group_member`;
-DROP TABLE `PREFIX_c3_mainfilter_selection_group`;
-DROP TABLE `PREFIX_c3_mainfilter_selection_part`;
-DROP TABLE `PREFIX_c3_mainfilter_selection`;
+DROP TABLE `ps_c3_mainfilter_selection_group_shelf`;
+DROP TABLE `ps_c3_mainfilter_selection_group_member`;
+DROP TABLE `ps_c3_mainfilter_selection_group`;
+DROP TABLE `ps_c3_mainfilter_selection_part`;
+DROP TABLE `ps_c3_mainfilter_selection`;

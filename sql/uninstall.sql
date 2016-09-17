71889d72ed3470d8af334f718fcbd215245c73ab
schneppd <david.schnepp@schneppd.com>
2016-09-17 16:07:01 +0200

    Create module's uninstall sql.

--- sql/uninstall.sql
+++ sql/uninstall.sql
@@ -1,1 +1,5 @@
-
+DROP TABLE `PREFIX_c3_mainfilter_selection_group_shelf`;
+DROP TABLE `PREFIX_c3_mainfilter_selection_group_member`;
+DROP TABLE `PREFIX_c3_mainfilter_selection_group`;
+DROP TABLE `PREFIX_c3_mainfilter_selection_part`;
+DROP TABLE `PREFIX_c3_mainfilter_selection`;

be17e1204eeb9d95ba1207435af7ce1bdaf656e1
schneppd <david.schnepp@schneppd.com>
2016-09-18 19:09:32 +0200

    Add model cache file for categories

--- src/module/mainfiltermodel.php
+++ src/module/mainfiltermodel.php
@@ -67,7 +67,28 @@ class MainFilterModel extends \NsC3MainFilterFramework\ModuleModel {
 	 * @return mixed[] the list of all selection values (multi dimension array) for given filter group
 	 */
 	public function getAllFilterGroupSelectionValues(&$id_filter_selection_group, &$id_lang) {
-		$sql = 'SELECT id_filter_selection_group, name FROM `' . $this->database->getDatabasePrefix() . 'c3_mainfilter_selection_group`';
-		return $this->database->getDatabaseInstance()->executeS($sql);
+		$res = array();
+		$sqlGroupMembers = 'SELECT id_filter_selection FROM `' . $this->database->getDatabasePrefix() . 'c3_mainfilter_selection_group_member` WHERE id_filter_selection_group = '.(int) $id_filter_selection_group;
+		$groupMembers = $this->database->getDatabaseInstance()->executeS($sqlGroupMembers);
+		foreach($groupMembers as $groupMember) {
+			$id_filter_selection = (int) $groupMember['id_filter_selection'];
+			$sqlFilterParts = 'SELECT id_feature, id_feature_value, name_feature, name_feature_value FROM `' . $this->database->getDatabasePrefix() . 'vc3_mainfilter_selection_part_informations` WHERE id_filter_selection = '.(int) $id_filter_selection.' AND id_lang = '. (int) $id_lang;
+			$filterParts = $this->database->getDatabaseInstance()->executeS($sqlFilterParts);
+			for($i = 0; $i < count($filterParts); $i++) {
+				$this->appendFilterPart($i, $filterParts, $res);
+			}
+		}
+		return $res;
+	}
+	
+	private function appendFilterPart(&$index, &$filterParts, &$currentContainer) {
+		$id_feature = (int) $filterParts[$index]['id_feature'];
+		$id_feature_value = (int) $filterParts[$index]['id_feature_value'];
+		//create new branch if it doesn't exist
+		if (!array_key_exists($id_feature, $currentContainer)) {
+			$name_feature = $filterParts[$index]['name_feature'];
+			$currentContainer[$id_feature] = array('name_feature' => $name_feature, 'values' => array());
+		}
+		$currentContainer[$bla] = 12;
 	}
 }

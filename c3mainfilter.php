bdbcf35a5625f919aa1b1fcaee8ea8e4b754a140
schneppd <david.schnepp@schneppd.com>
2016-10-29 11:41:06 +0200

    Start 0.2 changes to: add js header add menu in page header add ajax calls

--- c3mainfilter.php
+++ c3mainfilter.php
@@ -36,7 +36,7 @@ class C3MainFilter extends Module {
 		//setup this module's basic informations
 		$this->name = 'c3mainfilter';
 		$this->tab = 'front_office_features';
-		$this->version = '0.1.0';
+		$this->version = '0.2.0';
 		$this->author = 'Schnepp David';
 		$this->need_instance = 0;
 
